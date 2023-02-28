5626d667c014492a01a0afd2934bc31add273efb
qrave1 <liveartem@yandex.ru>
2023-02-28 16:35:51 +0300

    Added setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
 from setuptools import setup, find_packages
 
+with open('README.MD', 'r') as f:
+    long_description = f.read()
+
+with open('requirements.txt', 'r') as f:
+    requirements = f.read()
+requirements = requirements.split()
+
 setup(
     name='ToDo_App',
     version='1.0',
     packages=find_packages(),
     url='https://github.com/qrave1/ToDo_Web_App',
-    license='',
+    license='MIT',
     author='qrave1',
     author_email='liveartem@yandex.ru',
-    description='My ToDo web application'
+    description='My ToDo web application',
+    long_description=long_description,
+    install_requires=requirements,  # и это тоже
+    scripts=['app/main.py']  # не факт что правильно
 )
